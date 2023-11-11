859f85cafba551c3ec2d01507a41ae8ec56e31c2
kimdcottrell <me@kimdcottrell.com>
2023-11-11 03:11:29 -0500

    fixed loading issue

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	c "github.com/kimdcottrell/edgar/companies" // TODO: lazy load this as 'api'
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/companies", GetCompanies)
+	r.GET("/companies", c.GetCompanies)
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
